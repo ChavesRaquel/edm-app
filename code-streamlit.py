ea8d46521b8de07fbd74e9b7ac3b3b7575238e44
ChavesRaquel <95935022+ChavesRaquel@users.noreply.github.com>
2023-06-24 18:42:16 +0200

    Add files via upload

---
+++ code-streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import csv
+import pandas as pd
+import plotly.express as px
+import folium
+from streamlit_folium import st_folium
+from folium import plugins
+
+add_selectbox = st.sidebar.selectbox('Elige el tipo de transporte',('Coche','Moto','Bicicleta'))
+
+def abrir_html(filename):
+    with open(filename, "r") as file:
+        html_content = file.read()
+        return html_content
+    
+if add_selectbox == 'Bicicleta':
+    bicis = abrir_html('mapa-bicicletas.html')
+    st.components.v1.html(bicis, height=600)
+    
+elif add_selectbox == 'Moto':
+    moto = abrir_html('mapa-motos.html')
+    st.components.v1.html(moto, height=600)
+
+elif add_selectbox == 'Coche':
+    coches = abrir_html('mapa-coches.html')
+    st.components.v1.html(coches, height=600)
