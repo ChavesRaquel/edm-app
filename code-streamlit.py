7ba7963254acc7d98b02fbdcc0a9b54a310fb604
Raquel Chaves <racha1601@gmail.com>
2023-06-26 12:57:32 +0200

    Terminology changes

--- code-streamlit.py
+++ code-streamlit.py
@@ -6,7 +6,7 @@ import folium
 from streamlit_folium import st_folium
 from folium import plugins
 
-add_selectbox = st.sidebar.selectbox('Elige el tipo de transporte',('Coche','Moto','Bicicleta'))
+add_selectbox = st.sidebar.selectbox('Elige el tipo de transporte',('Turismo','Moto','Bicicleta'))
 
 def abrir_html(filename):
     with open(filename, "r") as file:
@@ -21,6 +21,6 @@ elif add_selectbox == 'Moto':
     moto = abrir_html('mapa-motos.html')
     st.components.v1.html(moto, height=600)
 
-elif add_selectbox == 'Coche':
+elif add_selectbox == 'Turismo':
     coches = abrir_html('mapa-coches.html')
     st.components.v1.html(coches, height=600)
